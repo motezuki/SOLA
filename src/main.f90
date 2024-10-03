ff55b262d1a8802b64417047eb427352c383a842
Fabio Motezuki <95715242+motezuki@users.noreply.github.com>
2024-10-03 18:06:29 +0000

    test

--- src/main.f90
+++ src/main.f90
@@ -1,16 +1,5 @@
-program soma
-    implicit none
-    real :: num1, num2, resultado
+program HelloWorld
 
-    ! Solicita ao usuário para inserir dois números
-    print *, 'Digite o primeiro número:'
-    read *, num1
-    print *, 'Digite o segundo número:'
-    read *, num2
+    print *, "Hello, World!"
 
-    ! Calcula a soma dos números
-    resultado = num1 + num2
-
-    ! Exibe o resultado
-    print *, 'A soma de ', num1, ' e ', num2, ' é:', resultado
-end program soma
+end program HelloWorld
